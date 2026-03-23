a3746bb7f62309245872ba129d1b55d386af7770
alnunezva <nunezvalvaro@gmail.com>
2026-03-23 16:12:07 -0300

    Limpiando public y forzando renderizado React 19

--- laciacoop-front-v2/src/index.js
+++ laciacoop-front-v2/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1 style={{ color: 'blue' }}>¡ESTOY VIVO! 🚀</h1>
-      <p>Si ves esto en azul, el Front está funcionando.</p>
-    </div>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
